dbd07c3a562e83a3467f839e701108d0d264b2f6
ozavcodez <akandeozavize@gmail.com>
2025-10-23 11:48:46 +0100

    updates

--- src/pages/shop/components/StationCard.tsx
+++ src/pages/shop/components/StationCard.tsx
@@ -21,7 +21,7 @@ export const StationCard = ({ role, isSelected, onClick }: StationCardProps) => 
       <CardContent className="p-4">
         <div className="space-y-3">
           <div className="flex items-center justify-between">
-            <h3 className={` ${isSelected  ? 'text-lg font-normal ' : 'font-bold text-[16px]'} text-black   `}>{role.name}</h3>
+            <h3 className={` ${isSelected  ? 'text-lg font-normal ' : 'font-bold text-[16px]'} text-black   `}>{role.workstationName}</h3>
             {/* <Badge 
             //   variant={role.status === 'Active' ? 'default' : 'secondary'}
               className={`${role.status === 'Active'  ? 'bg-[#0BC33F33] text-[#0BC33F]' : 'bg-[#ED143B33] text-[#ED143B]'} px-2 py-1 rounded-[50px] text-sm `}
