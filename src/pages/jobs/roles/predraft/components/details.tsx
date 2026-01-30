34bbfb00b68cc9cab1a31d454f9598de611d75f9
ozavcodez <akandeozavize@gmail.com>
2026-01-30 11:23:43 +0100

    updates

--- src/pages/jobs/roles/predraft/components/details.tsx
+++ src/pages/jobs/roles/predraft/components/details.tsx
@@ -137,13 +137,14 @@ export function PreDraftDetailsPage() {
         { label: 'Edge', value: fab.edge_name || 'N/A' },
         { label: 'Total square ft', value: String(fab.total_sqft) },
     ] : [];
+    
 
     const sidebarSections = [
-        // {
-        //     title: "Job Information",
-        //     type: "details",
-        //     items: jobInfo,
-        // },
+        {
+            title: "Template Information",
+            type: "details",
+            items: jobInfo,
+        },
         {
             title: "FAB Notes",
             type: "notes",
